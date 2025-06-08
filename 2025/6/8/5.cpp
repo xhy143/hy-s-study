7e46e49694e89ef64246d815a863217365328293
xhy17 <xhy174166@outlook.com>
2025-06-08 11:40:33 +0800

    https://zhuoyue.codechild.cn/p/2311(and less time)

--- 2025/6/8/5.cpp
+++ 2025/6/8/5.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int ans=0,n;
-int f(int x){
-    if(x>3){
-        return f(x-2)+f(x-3);
-    }
-    if(x==1){
+
+vector<int> visited(55,0);
+int f(int n){
+    if(n<0){
         return 0;
     }
-    if(x==2){
+    if(n==0){
         return 1;
     }
-    if(x==3){
-        return 1;
+    if(visited[n]!=0){
+        return visited[n];
     }
+    visited[n]=f(n-2)+f(n-3);
+    return visited[n];
 }
 int main(){
-    while(1){
-        cin>>n;
-        if((!n)) break;
-        cout<<f(n)<<endl;
-    }  
+    int n;
+    while(cin>>n&&n!=0){
+        int k=f(n);
+        cout<<k<<endl;
+    }
 }//2222 332 323 233
