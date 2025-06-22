5d6f10618df5d344667e7491f384c4219d4b7234
xhy17 <xhy174166@outlook.com>
2025-06-22 10:27:42 +0800

    https://zhuoyue.codechild.cn/p/50559

---
+++ 2025/6/22/5.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,ans=0;
+    cin>>n;
+    for(int i=1;i<=sqrt(n);i++){
+        if(n%i==0){
+            ans++;
+        }
+    }
+    cout<<ans;
+}
