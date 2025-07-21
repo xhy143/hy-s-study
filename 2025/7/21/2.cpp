ca714c48fa69a300f58176b683705e66dbdb650a
xhy143 <xhy174166@outlook.com>
2025-07-21 09:30:57 +0800

    https://zhuoyue.codechild.cn/p/1308

---
+++ 2025/7/21/2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> G;
+vector<int>vis(15,0);
+int n;
+void dfs(){
+    if(G.size()==n){
+        for(int i=0;i<G.size();i++){
+            cout<<G[i];
+            
+        }
+        cout<<endl;
+        return;
+    }
+    for(int i=1;i<=n;i++){
+        if(vis[i]==0){
+            vis[i]=1;
+            G.push_back(i);
+            dfs();
+            vis[i]=0;
+            G.pop_back();
+        }
+        
+    }
+}
+int main(){
+    cin>>n;
+    dfs();
+}
