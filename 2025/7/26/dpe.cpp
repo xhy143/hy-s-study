7ee1ca6e3bfb54d02645dca224d6f4badf5045e1
xhy143 <xhy174166@outlook.com>
2025-07-26 11:47:53 +0800

    ???

--- 2025/7/26/dpe.cpp
+++ 2025/7/26/dpe.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int a[100][100];
 int dp[100][100];
 int path[1001][1001];
+struct node{
+    int x,y;
+};
 int main(){
     int n;
     cin>>n;
@@ -17,7 +20,7 @@ int main(){
             if(dp[i+1][j]>=dp[i+1][j+1]){
                 dp[i][j]+=dp[i+1][j];
                 path[i][j]=j;
-            }
+            }   
             else {
                 dp[i][j]+=dp[i+1][j+1];
                 path[i][j]=j+1;
@@ -25,5 +28,20 @@ int main(){
             //dp[i][j]=a[i][j]+max(dp[i+1][j],dp[i+1][j+1]);
         }
     }
-    cout<<dp[1][1];
+    stack<node>st;
+    int row=1;
+    for(int i=1;i<=n;i++){
+        st.push({i,row});
+        row=path[i][row];
+    }
+    while(!st.empty()){
+        node f=st.top();
+        st.pop();
+        if(st.empty()){
+            cout<<f.x<<","<<f.y<<endl;
+            cout<<dp[1][1];
+            return 0;
+        }
+        cout<<f.x<<","<<f.y<<"->";
+    }
 }
