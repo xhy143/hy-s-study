ad14b238aaa6704b56c5176dc44debb9c56601ed
xhy143 <xhy174166@outlook.com>
2025-07-27 12:38:32 +0800

    ???

--- 2025/7/27/bigbigtest/C.cpp
+++ 2025/7/27/bigbigtest/C.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 int n;
 int main(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<i<<"*"<<j<<'='<<i*j<<' ';
-        }
-        cout<<endl;
-    }
+    int x=8,y=5;
+    y=x^y;
+    x=x^y;
+    y=x^y;
+    cout<<x<<y;
 }
