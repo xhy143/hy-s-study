771c93029240d67dd5e91631dfe42b171ed18446
xhy143 <xhy174166@outlook.com>
2025-08-15 08:50:43 +0800

    666 dfs

--- 2025/8/14/l.cpp
+++ 2025/8/14/l.cpp
@@ -1,9 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-int sd[10][10];
-void dfs(int n){
-    
-}
+int G[10][10],k=0;
+void dfs(int i,int j){
+    int ky[10];
+            if(G[i][j]==0){
+                for(int k=1;k<=9;k++){
+                     G[i][j]=k;
+                     bool flag=false;
+                     for(int h=0;h<9;h++){
+                        if(G[i][h]==k&&h!=j){
+                            flag=1;
+                        }
+                     }
+                     if(flag){
+                        continue;
+                     }
+                     for(int l=0;l<9;l++){
+                        if(G[l][j]==k&&l!=i){
+                            flag=1;
+                        }
+                     }
+                     if(flag){
+                        continue;
+                     }
+                     int ix=i-i%3;
+                     int iy=j-j%3;
+                     for(int gx=0;gx<3;gx++){
+                        for(int gy=0;gy<3;gy++){
+                            if(G[ix+gx][iy+gy]==k&&ix+gx!=i&&iy+gy!=j){
+                                flag=1;
+                            }
+                        }
+                     }
+                     if(flag){
+                        continue;
+                     }
+
+                }
+            }
+        }
 int main(){
     for(int i=0;i<9;i++){
         string a;
@@ -11,8 +46,9 @@ int main(){
         for(int j=0;j<9;j++){
             if(a[j]=='.'){
                 a[j]='0';
+                k++;
             }
-            sd[i][j]=a[j]-'0';
+            G[i][j]=a[j]-'0';
         }
     }
 }
