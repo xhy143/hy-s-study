0fcf1710cee635debcd3db5d594ac82d072745a5
xhy143 <xhy174166@outlook.com>
2025-10-08 17:39:37 +0800

    Ã͹¥csp-j

--- 2025/10/8/GD-007/uqe/uqe.cpp
+++ 2025/10/8/GD-007/uqe/uqe.cpp
@@ -1,27 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    long long i,j,n,m;
-    freopen("uqe.in","r",stdin);
-    freopen("uqe.out","w",stdout);
-    char f[101];
-    cin>>f;
-    sscanf(f,"(%lldx+%d)(%lldx+%lld)",&i,&j,&n,&m);
-    long long a=i*n,b=i*m+j*n,c=j*m,flag=0;
-    if(a==0&&b==0&&c==0){
+char f[1000001];
+int main()
+{
+    long long i, j, n, m;
+    freopen("uqe.in", "r", stdin);
+    freopen("uqe.out", "w", stdout);
+
+    cin >> f;
+    sscanf(f, "(%lldx+%lld)(%lldx+%lld)", &i, &j, &n, &m);
+    long long a = i * n, b = i * m + j * n, c = j * m, flag = 0;
+    if (a == 0 && b == 0 && c == 0)
+    {
         return 0;
     }
-    if(a!=0){
-        printf("%dx^2",a);
-        if(b!=0||c!=0){
-            cout<<"+";
+    if (a != 0)
+    {
+        if (a != 1)
+            printf("%dx^2", a);
+        else
+            cout << "x^2";
+        if (b != 0 || c != 0)
+        {
+            cout << "+";
         }
     }
-    if(b!=0){
-        cout<<b<<"x";
-        if(c!=0) cout<<"+";
+    if (b != 0)
+    {
+        if (b != 1)
+            cout << b << "x";
+        else
+            cout << "x";
+        if (c != 0)
+            cout << "+";
     }
-    if(c!=0){
-        cout<<c;
+    if (c != 0)
+    {
+        cout << c;
     }
 }
