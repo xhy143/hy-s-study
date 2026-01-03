3d8f226c9512a63e132b558b224bc67ab4510275
xhy143 <xhy174166@outlook.com>
2026-01-03 10:37:22 +0800

    7891789178917891789178917891789178917891789178917891789178917891789178917891

--- 2026/1/3/a.cpp
+++ 2026/1/3/a.cpp
@@ -1,16 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dp[10001][10001],w[10001],p[10001];
+int dp[200000],w[201],p[201];
 int main(){
     int n,k;
     cin>>k>>n;
-    for(int i=0;i<n;i++){
-        cin>>w[i]>>p[i];
+    for(int i=1;i<=n;i++){
+        cin>>w[i];
     }
-    for(int i=0;i<n;i++){
-        for(int j=k-1;j>=0;j--){
-            dp[i][j]=max(dp[i][j-1],dp[i-1][j-w[i]]+p[i]);
+    for(int i=1;i<=n;i++){
+        for(int j=k;j>=w[i];j--){
+                dp[j]=max(dp[j],dp[j-w[i]]+w[i]);
+            
         }
     }
-    cout<<dp[n-1][k];
+    cout<<(dp[k]==k?"YES":"NO");
 }
