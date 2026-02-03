6348ae314cd41c8ffc866e2ff888567ffb19dafd
xhy143 <xhy174166@outlook.com>
2026-02-03 16:09:34 +0800

    ???

--- 2026/2/3/f.cpp
+++ 2026/2/3/f.cpp
@@ -28,7 +28,7 @@ int main(){
     int k;
     cin >> k;
     vector<int> a(n);
-    for (int i = 0; i < n; i++){//
+    for (int i = 0; i < n; i++){
         cin >> a[i];
     }
     for (int i = k; i < n; i++) {
