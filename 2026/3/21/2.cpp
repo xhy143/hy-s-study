ce43e017be429d848113176b4a9a4de2ff70681c
xhy143 <xhy174166@outlook.com>
2026-03-21 22:12:54 +0800

    ???

--- 2026/3/21/2.cpp
+++ 2026/3/21/2.cpp
@@ -1,35 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll l,r,dp[20][200][200];
-int len,a[20],mod;
-ll dfs(int pos,int sum,ll st,int limit)
+#define ll long long
+
+const int N = 15;
+ll f[N][2][N][2];
+int num[N];  
+ll dfs(int len, bool issmall, int sum, bool zero, int d)
 {
-	if(pos>len&&sum==0) return 0;
-	if(pos>len) return st==0&&sum==mod?1:0;
-	if(!limit&&dp[pos][sum][st]!=-1) return dp[pos][sum][st];
-	ll ret=0;
-	int res=limit?a[len-pos+1]:9;
-	for(int i=0;i<=res;i++)
-		ret+=dfs(pos+1,sum+i,(10ll*st+i)%mod,i==res&&limit);
-	return limit?ret:dp[pos][sum][st]=ret;
-}
-ll s(ll x)
-{
-	len=0;
-	while(x) a[++len]=x%10,x/=10;
-	ll ret=0;
-	for(mod=1;mod<=9*len;mod++)
-	{
-		memset(dp,-1,sizeof dp);
-	    ret+=dfs(1,0,0,1);
-	}
-	return ret;
-}
-int main()
-{
-    cin>>l>>r;
-    cout<<s(r)-s(l-1);
-	return 0;
+    ll ret = 0;
+    if (len == 0) return sum;  
+    if (f[len][issmall][sum][zero] != -1) return f[len][issmall][sum][zero];  
+    for (int i = 0; i < 10; i ++){
+        if (!issmall && i > num[len]) break;
+        ret += dfs(len-1, issmall || (i<num[len]), sum+((!zero || i) && (i==d)), zero && (i == 0), d);
+    }
+    f[len][issmall][sum][zero] = ret;
+    return ret;
 }
 
+ll solve(ll x, int d)
+{
+    int len = 0;
+    while (x){
+        num[++ len] = x%10;
+        x /= 10;
+    } 
+    memset(f, -1, sizeof f); 
+    return dfs(len, 0, 0, 1, d); 
+}
+
+int main()
+{
+    ll a, b; 
+    cin>>a>>b;
+    for (int i = 0; i < 10; i ++)
+        cout<< solve(b, i)-solve(a-1, i)<<' ';
+    return 0;
+}
